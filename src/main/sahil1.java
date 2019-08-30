2f4b65ce9c0bdb25c4c6321cb2f1b59571778c9f
SSTR2000 <sahil.tomar_cs18@gla.ac.in>
2019-08-31 00:06:44 +0530

    finally answer is found

--- src/main/sahil1.java
+++ src/main/sahil1.java
@@ -6,8 +6,11 @@ public class sahil1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double a = scanner.nextDouble();
-        double b = Math.ceil(a);
-        System.out.print(b + "ruppees and");
+        double b = (int) (a);
+        double c = (double) ((a - b) * 100);
+        double d = Math.ceil(c);
+        System.out.print(b + "ruppees and ");
+        System.out.println(d + "paise");
 
 
     }
