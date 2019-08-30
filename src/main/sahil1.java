d9b2c8e009113f0347abe47a4db3e02c58549eb2
SSTR2000 <sahil.tomar_cs18@gla.ac.in>
2019-08-30 23:14:02 +0530

    class:Scanner object:scanner new variable float is added

---
+++ src/main/sahil1.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.Scanner;
+
+public class sahil1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = Math.ceil(a);
+        System.out.print(b + "ruppees and");
+
+
+    }
+
+}
+
---
+++ src/main/sahil2.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.Scanner;
+
+public class sahil2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b;
+        b = (a - 32) / 1.8;
+        System.out.println(b);
+
+    }
+}
